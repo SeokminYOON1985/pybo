c78566ba658df61c8bf632ce479d6cdf91ce5284
SeokminYOON1985 <ysm6514@naver.com>
2023-10-31 06:11:54 +0900

    domain apli

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['43.202.175.207']
+ALLOWED_HOSTS = ['43.202.175.207', 'areasj.store']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
