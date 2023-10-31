cd22bfdc05122631460e1e02c281644b77065d7f
SeokminYOON1985 <ysm6514@naver.com>
2023-11-01 04:49:47 +0900

    database create

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,15 @@ ALLOWED_HOSTS = ['43.202.175.207', 'areasj.store']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD':'sm1803hr!',
+        'HOST': 'ls-fd0598da9bec1fbec48f244e35051fe129c9d6b2.ch9j4fdaqlmn.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+
+}
